a4a40b8df7bd42c63c715b7f30b8292e9b233fc1
Rreddddd <54348820+Rreddddd@users.noreply.github.com>
2023-01-28 12:40:55 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,8 +1,9 @@
 #！/bin/bash
-echo "选择梯子版本"
+echo "开始处理梯子"
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
 systemctl start docker
 docker pull rreddd/v2ray:$0
 docker run -idt --name=v2ray_$0 --privileged=true -p 443:443 rreddd/v2ray:$0 /usr/sbin/init
+echo "梯子搭建完成，版本号:$0"
