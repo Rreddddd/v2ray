9a40b71d4caf994fff6221c0f879bf7ffcb9083d
Rreddddd <54348820+Rreddddd@users.noreply.github.com>
2023-01-28 12:26:29 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,9 +1,8 @@
 #！/bin/bash
 echo "选择梯子版本"
-read ver
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
 systemctl start docker
-docker pull rreddd/v2ray:"${ver}"
-docker run -idt --name=v2ray_"${ver}" --privileged=true -p 443:443 rreddd/v2ray:"${ver}" /usr/sbin/init
+docker pull rreddd/v2ray:$0
+docker run -idt --name=v2ray_$0 --privileged=true -p 443:443 rreddd/v2ray:$0 /usr/sbin/init
