44dfb9cbda0da8ec3918d78f10a980888628983a
Rreddddd <54348820+Rreddddd@users.noreply.github.com>
2023-04-13 01:14:01 +0800

    Update reMove.sh

--- reMove.sh
+++ reMove.sh
@@ -2,21 +2,21 @@
 echo "参数1：$1"
 echo "参数2：$2"
 
-baseDir="/content/drive/MyDrive/veiod/$1/"
+baseDir="/content/drive/MyDrive/veiod/"
 # baseDir="/usr/local/$1/"
 function read_dir(){
-	lastNum=$((10#$2+0))
+	lastNum=$((10#$3+0))
 	echo "lastNum:"$lastNum
-	targetDir=$1
+	targetDir=$1$2
 	echo "$targetDir"
 	for file in `ls $targetDir`
 	do
 		fileLeft=${file%%.*}
 		fileLeft2=$((10#${fileLeft}+0))
 		if [ $fileLeft2 -le $lastNum ]; then
-			mv $targetDir$file $1$3"/"$fileLeft".png"
+			mv $targetDir$file $1$4"/"$fileLeft".png"
 		fi
 	done
 }
 
-read_dir $baseDir $2 $3
+read_dir $baseDir $1 $2 $3
